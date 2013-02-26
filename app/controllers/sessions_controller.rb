4236800d8f99772e1098de43ac4704a9e54509b7
Nick <nsdmccrea@gmail.com>
2013-02-25 22:43:28 -0600

    login works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,22 @@
 class SessionsController < ApplicationController
   
   def sign_in
+    if user_is_logged_in
+      flash[:notice] = 'You are already logged in.'
+      redirect_to user_path( current_user )
+    end
   end
   
   def submit_sign_in
+    user = User.where( params[:user] ).first
+    
+    if user
+      login( user )
+      redirect_to user_path( user )
+    else
+      flash[:alert] = 'Invalid username or password.'
+      redirect_to sign_in_path
+    end
   end
   
   def sign_out
