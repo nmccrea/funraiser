060e96881ee3af1844f14c0f68415077fcc3469e
Nick <nsdmccrea@gmail.com>
2013-02-25 22:04:34 -0600

    made a proxy method of user_is_logged_in for ApplicationHelper, to be used in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 module ApplicationHelper
   
+  @user_is_logged_in = nil
+  
   def body_tag_class
     "#{controller_name}-#{action_name}"
+  end
+  
+  def user_is_logged_in
+    @user_is_logged_in ||= controller.user_is_logged_in
+    @user_is_logged_in
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -10,4 +10,23 @@ describe ApplicationHelper do
       helper.body_tag_class.should == 'fundraisers-index'
     end
   end # describe '#body_tag_class'
+  
+  describe '#user_is_logged_in' do
+    
+    it 'should call the user_is_logged_in method on the controller and return true if that method returns true' do
+      controller.should_receive( :user_is_logged_in ).and_return( true )
+      helper.user_is_logged_in.should == true
+    end
+    
+    it 'should call the user_is_logged_in method on the controller and return false if that method returns false' do
+      controller.should_receive( :user_is_logged_in ).and_return( false )
+      helper.user_is_logged_in.should == false
+    end
+    
+    it 'should memoize the response from the controller for the duration of this request, so the controller is only asked once' do
+      controller.should_receive( :user_is_logged_in ).once.and_return( true )
+      helper.user_is_logged_in.should == true
+      helper.user_is_logged_in.should == true
+    end
+  end # describe '#user_is_logged_in'
 end
